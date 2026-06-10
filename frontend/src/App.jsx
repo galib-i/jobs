052d06a0b352d62b40e67941afc88f2a19ddeebe
Galib <217484167+galib-i@users.noreply.github.com>
2026-06-10 21:41:18 +0100

    chore: setup Wails with React, Vite and Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { Events, WML } from "@wailsio/runtime";
+import { GreetService } from "../bindings/changeme";
+
+function App() {
+  return <div className="text-red-400 font-bold text-9xl">Hello World</div>;
+}
+
+export default App;
